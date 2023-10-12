19e7aea4359e0071f9cd0d977acd39d03c06347e
HugooJr <hugoemanuelmonzoncarrasco@gmail.com>
2023-10-12 12:42:42 -0400

    Update webcontroller.js

--- src/controllers/webcontroller.js
+++ src/controllers/webcontroller.js
@@ -12,5 +12,23 @@ exports.datosAgregados= async (req, res) => {
       } = req.body;
 
       
-    LogicaNegocios.datosAgregados(frase,reaccion,colorfondo,colorLetra,ip)
-  };
+    try {
+      const resultado = await LogicaNegocios.datosAgregados(
+        frase,
+        reaccion,
+        colorfondo,
+        colorLetra,
+        ip,
+        //cantidadClicks,
+      );
+
+      // El controlador solo necesita comprobar el mensaje de éxito
+      if (resultado === '¡Registro guardado correctamente!') {
+        res.json({ mensaje: resultado });
+      } else {
+        res.json({ mensaje: resultado });
+      }
+    } catch (error) {
+      res.json({ mensaje: error.message });
+    }
+};
